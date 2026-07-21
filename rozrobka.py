c5bf614b72c3ff1c52deed574e92f0b86591c619
lera2221 <valeriia.savchenko.25@kse.org.ua>
2026-07-21 14:22:28 +0300

    signal column added + for

--- rozrobka.py
+++ rozrobka.py
@@ -7,12 +7,16 @@ stocks = "CROX"
 df = yf.download(stocks,start='2020-01-01',end='2025-12-31')
 #print(df)
 
-
-
-
 df["MA20"] = df["Close"].rolling(window=20).mean()
-df"MA50"] = df["Close"].rolling(window=50).mean()
+df["MA50"] = df["Close"].rolling(window=50).mean()
 
+df["Signal"] = 0
+
+for i in range(len(df)):
+    if df["MA20"][i] > df["MA50"][i]:
+        df["Signal"][i] = 1
+    elif df["MA20"][i] < df["MA50"][i]:
+        df["Signal"][i] = -1
 
 
 
