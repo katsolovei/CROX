de50a1b84ad0f16ff3de6156625bdad232169368
lera2221 <valeriia.savchenko.25@kse.org.ua>
2026-07-21 14:12:14 +0300

    df dates upd +print

--- rozrobka.py
+++ rozrobka.py
@@ -4,7 +4,10 @@ import matplotlib.pyplot as plt
 
 stocks = "CROX"
 
-df = yf.download(stocks,start='2024-01-01',end='2025-12-31')
+df = yf.download(stocks,start='2020-01-01',end='2025-12-31')
+#print(df)
+
+
 
 
 
