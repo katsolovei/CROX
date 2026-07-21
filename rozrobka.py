8664bd7438410f99301e06b8cd54127ad4677ebf
lera2221 <valeriia.savchenko.25@kse.org.ua>
2026-07-21 14:54:11 +0300

    new for through list signals

--- rozrobka.py
+++ rozrobka.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 stocks = "CROX"
 
 df = yf.download(stocks,start='2020-01-01',end='2025-12-31')
+df = df.reset_index()
+df.columns = df.columns.get_level_values(0)
 #print(df)
 
 for column in df.columns:
@@ -17,13 +19,19 @@ for column in df.columns:
 df["MA20"] = df["Close"].rolling(window=20).mean()
 df["MA50"] = df["Close"].rolling(window=50).mean()
 
-df["Signal"] = 0
+signals = []
 
 for i in range(len(df)):
     if df["MA20"][i] > df["MA50"][i]:
-        df["Signal"][i] = 1
+        signals.append(1)
     elif df["MA20"][i] < df["MA50"][i]:
-        df["Signal"][i] = -1
+        signals.append(-1)
+    else:
+        signals.append(0)
+
+df["Signal"] = signals
+
+print(df)
 
 
 
